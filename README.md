c323ca7f8be80004b5f7aa03620ac9bb4270bccb
sylvain05 <factory05@gmail.com>
2016-09-29 19:35:47 +0200

    cv amelioré

---
+++ README.md
@@ -0,0 +1,3 @@
+# cvsylvain
+# cvsylvain
+https://htmlpreview.github.io/?https://github.com/sylvain66/cvsylvain/blob/master/index.html
---
+++ jquery.js
@@ -0,0 +1,18 @@
+	$(".acacher").click(function(){
+	$("ul.ulformation").fadeIn(1500).delay(2000).fadeOut(1500);
+	$("ul.ulformation").animation("fontwize:35px","paddingtop:50px")
+	});
+
+
+
+	$(".professionelle").click(function(){
+	$("ul.ulprofessionelle").fadeIn(1500).delay(2800).fadeOut(1500);	
+
+	});
+
+	$(".centre").click(function(){
+		$("ul.ulcentre").fadeIn(1500).delay(2000).fadeOut(1500);	
+
+
+	});
+	
